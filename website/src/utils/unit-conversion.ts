63b1bccf0893d168402199dc30f57f18281597bc
the-r3aper7 <saisrikardumpeti@gmail.com>
2023-06-01 01:44:58 +0530

    added unit-conversions

--- website/src/utils/unit-conversion.ts
+++ website/src/utils/unit-conversion.ts
@@ -1,10 +1,9 @@
 export const formatBytes = (bytes: number | undefined): string => {
-
-  const sizes = ["B", "KB", "MB", "GB", "TB"];
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
   if (bytes === 0 || !bytes) {
-    return "0 B";
+    return '0 B';
   }
 
   const i = Math.floor(Math.log(bytes) / Math.log(1024));
   return `${parseFloat((bytes / Math.pow(1024, i)).toFixed(2))} ${sizes[i]}`;
-}
+};
